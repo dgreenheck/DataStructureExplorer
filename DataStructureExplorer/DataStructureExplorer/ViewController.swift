28b301476213a6c7a179b555dade6ecd4729bd96
Daniel Greenheck <dgreenheck@gmail.com>
2019-10-30 16:16:44 -0700

    Initial commit

---
+++ DataStructureExplorer/DataStructureExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataStructureExplorer
+//
+//  Created by Daniel Greenheck on 10/30/19.
+//  Copyright © 2019 Daniel Greenheck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
